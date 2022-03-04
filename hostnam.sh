9e0d83efe68e5411ae745d28b5db5fb838f271a2
sevenhillsacademy <100567650+sevenhillsacademy@users.noreply.github.com>
2022-03-04 07:04:57 +0530

    Update hostnam.sh

--- hostnam.sh
+++ hostnam.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-# Get the Linux Hostname
+mkdir devenv
+cd devenv
+touch file1.txt file2.txt file3.txt file4.txt
+echo "welcome to DevOps world" > file1.txt
+
+# Get the Hostname of the Server
+echo "Hostname of the Server :"
 hostname
+
+# Get the User name
+echo "User name :"
 whoami
+
+# Get the version from server
+echo "Version of the server :"
+uname -r
+
+# Get the Ip address of the server
+echo "Ip of the server :"
+ifconfig
